8441fd0e6306122d6190b4e3b9797cc145dba37a
Allenyhf <1347208009@qq.com>
2021-07-06 23:27:41 +0800

    rewrite TestArrayDequeGold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -8,73 +8,33 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> studentADeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solutionADeque = new ArrayDequeSolution<>();
 
-        for (int i = 0; i < 500; i++) {
-            double randomNumberzero2one = StdRandom.uniform();
-            if (randomNumberzero2one < 0.25) {
-                studentADeque.addFirst(i);
-                solutionADeque.addFirst(i);
-            } else if (randomNumberzero2one < 0.5) {
-                studentADeque.addLast(i);
-                solutionADeque.addLast(i);
-            } else if (randomNumberzero2one < 0.75) {
-//                if (studentADeque.size() != solutionADeque.size()) {
-//                    System.out.println("Your solution is not correct: ");
-//                    System.out.println("the size should be: " + solutionADeque.size());
-//                    System.out.println("but it is: " + studentADeque.size());
-//                    break;
-//                } else
-                int studentSize = studentADeque.size();
-                int solutionSize = solutionADeque.size();
-                assertEquals("size()\n",
-                                studentSize, solutionSize);
-
-                // if (studentADeque.size() != 0) {
-                    Integer tmpStudent;
-//                    tmpStudent = studentADeque.removeFirst();
-                    if (studentADeque.size() > 0) {
-                        tmpStudent = studentADeque.removeFirst();
-                    } else {
-                        tmpStudent = null;
-                    }
-                    Integer tmpSolution;
-//                    tmpSolution = solutionADeque.removeFirst();
-                    if (solutionADeque.size() > 0) {
-                        tmpSolution = solutionADeque.removeFirst();
-                    } else {
-                        tmpSolution = null;
-                    }
-                        assertEquals("removeFirst()\n",
-                            tmpSolution, tmpStudent);
-              //  }
+        for (int i = 0; i < 100; i++) {
+            double random = StdRandom.uniform();
+            if (random < 0.5) {
+                int num = (int) random * i * 100;
+                studentADeque.addFirst(num);
+                solutionADeque.addFirst(num);
             } else {
-                int studentSize = studentADeque.size();
-                int solutionSize = solutionADeque.size();
-                assertEquals("size()\n",
-                        studentSize, solutionSize);
-
-                //if (studentADeque.size() != 0) {
-
-                    Integer tmpStudent;
-//                    tmpStudent = studentADeque.removeLast();
-                    if (studentADeque.size() > 0) {
-                        tmpStudent = studentADeque.removeLast();
-                    } else {
-                        tmpStudent = null;
-                    }
-                    Integer tmpSolution;
-//                    tmpSolution = solutionADeque.removeLast();
-                    if (solutionADeque.size() > 0) {
-                        tmpSolution = solutionADeque.removeLast();
-                    } else {
-                        tmpSolution = null;
-                    }
-                    assertEquals("removeLast()\n",
-                            tmpSolution, tmpStudent);
-              //  }
-
+                int num = (int) (1 - random) * i * 100;
+                studentADeque.addLast(num);
+                solutionADeque.addLast(num);
             }
         }
+
+        for(int i = 0; i < 100; i++) {
+            double random = StdRandom.uniform();
+            if (random < 0.5) {
+                Integer student = studentADeque.removeFirst();
+                Integer solution = solutionADeque.removeFirst();
+                assertEquals("removeFirst()\n", student, solution);
+            } else {
+                Integer student = studentADeque.removeLast();
+                Integer solution = solutionADeque.removeLast();
+                assertEquals("removeLast()\n", student, solution);
+            }
+        }
+
     }
 
-
 }
+
