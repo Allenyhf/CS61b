bb5b1c79e7b2cbcd1553fec79a3033ad3d4174ab
Allenyhf <1347208009@qq.com>
2021-07-15 16:21:30 +0800

    Fix style

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,13 @@
+package synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T>{
+    protected int capacity;
+    protected int fillCount;
+    public int capacity() {
+        return capacity;
+    }
+    public int fillCount() {
+        return fillCount;
+    }
+
+}
--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,30 +1,28 @@
-// TODO: Make sure to make this class a part of the synthesizer package
+
 package synthesizer;
 import java.util.Iterator;
 
-//TODO: Make sure to make this class and all of its methods public
-//TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T> implements BoundedQueue<T>{
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>{
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
     private int last;
     /* Array for storing the buffer data. */
     private T[] rb;
-    private int fillCount;
-    private int cap;
+   // private int fillCount;
+   // private int cap;
 
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
 
     public ArrayRingBuffer(int capacity) {
-        // TODO: Create new array with capacity elements.
+        //       Create new array with capacity elements.
         //       first, last, and fillCount should all be set to 0.
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
-        cap = capacity;
+        this.capacity = capacity;
         rb =(T[]) new Object[capacity];
         first = 0;
         last = 0;
@@ -38,13 +36,13 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T>{
      */
     @Override
     public void enqueue(T x) {
-        // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        // Enqueue the item. Don't forget to increase fillCount and update last.
         if (fillCount == capacity()) {
             throw new RuntimeException("Ring Buffer overflow");
         }
 
         rb[last] = x;
-        last = (last + 1) % cap;
+        last = (last + 1) % capacity;
         fillCount++;
     }
 
@@ -55,12 +53,12 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T>{
      */
     @Override
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        // Dequeue the first item. Don't forget to decrease fillCount and update
         if (fillCount == 0) {
             throw new RuntimeException("Ring Buffer underflow");
         }
         T tmp = rb[first];
-        first = (first + 1) % cap;
+        first = (first + 1) % capacity;
         fillCount--;
         return tmp;
     }
@@ -70,28 +68,28 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T>{
      */
     @Override
     public T peek() {
-        // TODO: Return the first item. None of your instance variables should change.
+        // Return the first item. None of your instance variables should change.
         return rb[first];
     }
     /**
      * Return size of the buffer
      */
-    @Override
-    public int capacity() {
-        return cap;
-    }
+//    @Override
+//    public int capacity() {
+//        return capacity;
+//    }
 
     /*
      * Return number of items currently in the buffer
      */
-    @Override
-    public int fillCount() {
-        return fillCount;
-    }
+//    @Override
+//    public int fillCount() {
+//        return fillCount;
+//    }
 
 
 
-    // TODO: When you get to part 5, implement the needed code to support iteration.
+    // When you get to part 5, implement the needed code to support iteration.
 
     public Iterator<T> iterator() {
         return new ARBIterator<T>();
@@ -111,7 +109,7 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T>{
         @Override
         public T next() {
             T tmp = (T) rb[ptr];
-            ptr = (ptr + 1) % cap;
+            ptr = (ptr + 1) % capacity;
             return tmp;
         }
     }
@@ -137,7 +135,7 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T>{
                 return false;
             }
             ele1 = rb[ptr];
-            ptr = (ptr + 1) % cap;
+            ptr = (ptr + 1) % capacity;
             if (! ele2.equals(ele1)) {
                 return false;
             }
